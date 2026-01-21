a4221b4cee233466a7768c7235f1227b3e2c8c71
chenxuwu <chenxuwu@tencent.com>
2026-01-21 16:37:55 +0800

    暂时去掉代理：taiji平台上代理有问题

--- run_multienv_uitars15_v2.py
+++ run_multienv_uitars15_v2.py
@@ -180,7 +180,7 @@ def run_env_tasks(task_queue: Queue, args: argparse.Namespace, shared_scores: li
             headless=args.headless,
             os_type="Ubuntu",
             require_a11y_tree=args.observation_type in ["a11y_tree", "screenshot_a11y_tree", "som"],
-            enable_proxy=True,
+            enable_proxy=False,
             client_password=args.client_password
         )
         active_environments.append(env)
